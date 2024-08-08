232a610b1b75c941d09d64371a06a0f5c79c104a
Lucassamuel97 <lukassamuka88@gmail.com>
2024-08-08 12:21:59 -0300

    created customer api endpoint

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -2,32 +2,176 @@ import { app, sequelize } from "../express";
 import request from "supertest";
 
 describe("E2E test for customer", () => {
-    beforeEach(async () => {
-      await sequelize.sync({ force: true });
+  beforeEach(async () => {
+    await sequelize.sync({ force: true });
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a customer", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "samuca",
+        address: {
+          street: "Street",
+          city: "City",
+          number: 123,
+          zip: "12345",
+        },
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe("samuca");
+    expect(response.body.address.street).toBe("Street");
+    expect(response.body.address.city).toBe("City");
+    expect(response.body.address.number).toBe(123);
+    expect(response.body.address.zip).toBe("12345");
+  });
+
+  it("should not create a customer", async () => {
+    const response = await request(app).post("/customer").send({
+      name: "Samuca",
     });
-  
-    afterAll(async () => {
-      await sequelize.close();
-    });
-  
-    it("should create a customer", async () => {
-      const response = await request(app)
-        .post("/customer")
-        .send({
-          name: "samuca",
-          address: {
-            street: "Street",
-            city: "City",
-            number: 123,
-            zip: "12345",
-          },
-        });
-  
-      expect(response.status).toBe(200);
-      expect(response.body.name).toBe("samuca");
-      expect(response.body.address.street).toBe("Street");
-      expect(response.body.address.city).toBe("City");
-      expect(response.body.address.number).toBe(123);
-      expect(response.body.address.zip).toBe("12345");
-    });
+    expect(response.status).toBe(500);
+  });
+
+  it("should update a customer", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "Samuca",
+        address: {
+          street: "Street",
+          city: "City",
+          number: 123,
+          zip: "12345",
+        },
+      });
+    expect(response.status).toBe(200);
+
+    const updateResponse = await request(app)
+      .put("/customer/" + response.body.id)
+      .send({
+        id: response.body.id,
+        name: "Samuca Update",
+        address: {
+          street: "Street Update",
+          city: "City 2",
+          number: 1234,
+          zip: "12344",
+        },
+      });
+    expect(updateResponse.status).toBe(200);
+    expect(updateResponse.body.name).toBe("Samuca Update");
+    expect(updateResponse.body.address.street).toBe("Street Update");
+  });
+
+  it("should not update a customer", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "Samuca",
+        address: {
+          street: "Street",
+          city: "City",
+          number: 123,
+          zip: "12345",
+        },
+      });
+    expect(response.status).toBe(200);
+
+    const updateResponse = await request(app)
+      .put("/customer/" + response.body.id)
+      .send({
+        name: "Samuca Update",
+      });
+    expect(updateResponse.status).toBe(500);
+
+  });
+
+  it("should find a customer by id", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "Samuca",
+        address: {
+          street: "Street",
+          city: "City",
+          number: 123,
+          zip: "12345",
+        },
+      });
+    expect(response.status).toBe(200);
+
+    const findResponse = await request(app)
+      .get("/customer/" + response.body.id)
+      .send();
+    expect(findResponse.status).toBe(200);
+    expect(findResponse.body.name).toBe("Samuca");
+    expect(findResponse.body.address.street).toBe("Street");
+  });
+
+
+  it("should not find a customer by id", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "Samuca",
+        address: {
+          street: "Street",
+          city: "City",
+          number: 123,
+          zip: "12345",
+        },
+      });
+    expect(response.status).toBe(200);
+
+    const findResponse = await request(app)
+      .get("/customer/" + "123")
+      .send();
+    expect(findResponse.status).toBe(500);
+  });
+
+  it("should list all customer", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "Samuca",
+        address: {
+          street: "Street",
+          city: "City",
+          number: 123,
+          zip: "12345",
+        },
+      });
+    expect(response.status).toBe(200);
+    const response2 = await request(app)
+      .post("/customer")
+      .send({
+        name: "Jane",
+        address: {
+          street: "Street 2",
+          city: "City 2",
+          number: 1234,
+          zip: "12344",
+        },
+      });
+    expect(response2.status).toBe(200);
+
+    const listResponse = await request(app).get("/customer").send();
+
+    expect(listResponse.status).toBe(200);
+    expect(listResponse.body.customers.length).toBe(2);
+    const customer = listResponse.body.customers[0];
+    expect(customer.name).toBe("Samuca");
+    expect(customer.address.street).toBe("Street");
+    const customer2 = listResponse.body.customers[1];
+    expect(customer2.name).toBe("Jane");
+    expect(customer2.address.street).toBe("Street 2");
+  });
+
+
 });
--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
 import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
+import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usecase";
+import FindCustomerUseCase from "../../../usecase/customer/find/find.customer.usecase";
 
 export const customerRoute = express.Router();
 
@@ -17,6 +19,46 @@ customerRoute.post("/", async (req: Request, res: Response) => {
             },
         };
         const output = await usecase.execute(customerDto);
+        res.send(output);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+customerRoute.put("/:id", async (req: Request, res: Response) => {
+    const usecase = new CreateCustomerUseCase(new CustomerRepository());
+    try {
+        const customerDto = {
+            id: req.params.id,
+            name: req.body.name,
+            address: {
+                street: req.body.address.street,
+                city: req.body.address.city,
+                number: req.body.address.number,
+                zip: req.body.address.zip,
+            },
+        };
+        const output = await usecase.execute(customerDto);
+        res.send(output);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+customerRoute.get("/:id", async (req: Request, res: Response) => {
+    const usecase = new FindCustomerUseCase(new CustomerRepository());
+    try {
+        const output = await usecase.execute({ id: req.params.id });
+        res.send(output);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+customerRoute.get("/", async (req: Request, res: Response) => {
+    const usecase = new ListCustomerUseCase(new CustomerRepository());
+    try {
+        const output = await usecase.execute({});
         res.send(output);
     } catch (err) {
         res.status(500).send(err);
