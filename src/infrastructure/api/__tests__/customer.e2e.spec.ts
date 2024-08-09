8acf00639b924ba094f7db5ef9a19b7a9f6c8e58
Lucassamuel97 <lukassamuka88@gmail.com>
2024-08-09 13:24:42 -0300

    I created presenter for customer list

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -173,5 +173,51 @@ describe("E2E test for customer", () => {
     expect(customer2.address.street).toBe("Street 2");
   });
 
+  it("should list all xml customer", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "Samuca",
+        address: {
+          street: "Street",
+          city: "City",
+          number: 123,
+          zip: "12345",
+        },
+      });
+    expect(response.status).toBe(200);
+    const response2 = await request(app)
+      .post("/customer")
+      .send({
+        name: "Jane",
+        address: {
+          street: "Street 2",
+          city: "City 2",
+          number: 1234,
+          zip: "12344",
+        },
+      });
+    expect(response2.status).toBe(200);
 
+    const listResponseXML = await request(app).get("/customer/")
+    .set("Accept", "application/xml")
+    .send();
+
+    expect(listResponseXML.status).toBe(200);
+    expect(listResponseXML.text).toContain(`<?xml version="1.0" encoding="UTF-8"?>`);
+    expect(listResponseXML.text).toContain(`<customers>`);
+    expect(listResponseXML.text).toContain(`<customer>`);
+    expect(listResponseXML.text).toContain(`<name>Samuca</name>`);
+    expect(listResponseXML.text).toContain(`<address>`);
+    expect(listResponseXML.text).toContain(`<street>Street</street>`);
+    expect(listResponseXML.text).toContain(`<city>City</city>`);
+    expect(listResponseXML.text).toContain(`<number>123</number>`);
+    expect(listResponseXML.text).toContain(`<zip>12345</zip>`);
+    expect(listResponseXML.text).toContain(`</address>`);
+    expect(listResponseXML.text).toContain(`</customer>`);
+    expect(listResponseXML.text).toContain(`<name>Jane</name>`);
+    expect(listResponseXML.text).toContain(`<street>Street 2</street>`);
+    expect(listResponseXML.text).toContain(`</customers>`);
+    
+  });
 });
